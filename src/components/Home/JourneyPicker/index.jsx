0419720918f9968e1247eb77f43bf24b97039ffe
Šárka Princová Maroušková <sarka.marouskova@gmail.com>
2021-05-22 12:49:13 +0200

    Údaje o cestě

--- src/components/Home/JourneyPicker/index.jsx
+++ src/components/Home/JourneyPicker/index.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import mapImage from './img/map.svg';
 import './style.css';
 
 export const JourneyPicker = () => {
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('Odesílám formulář s cestou');
+    console.log({ fromCity });
+    console.log({ toCity });
+    console.log({ date });
+  };
+
+  const [fromCity, setFromCity] = useState('');
+  const [toCity, setToCity] = useState('');
+  const [date, setDate] = useState('');
+
   return (
-    <div class="journey-picker container">
-      <h2 class="journey-picker__head">Kam chcete jet?</h2>
-      <div class="journey-picker__body">
-        <form class="journey-picker__form">
+    <div className="journey-picker container">
+      <h2 className="journey-picker__head">Kam chcete jet?</h2>
+      <div className="journey-picker__body">
+        <form onSubmit={handleSubmit} className="journey-picker__form">
           <label>
-            <div class="journey-picker__label">Odkud:</div>
-            <select>
+            <div className="journey-picker__label">Odkud:</div>
+            <select
+              value={fromCity}
+              onChange={(event) => setFromCity(event.target.value)}
+            >
               <option value="">Vyberte</option>
               <option value="Mesto1">Město 1</option>
               <option value="Mesto2">Město 2</option>
@@ -19,8 +34,11 @@ export const JourneyPicker = () => {
             </select>
           </label>
           <label>
-            <div class="journey-picker__label">Kam:</div>
-            <select>
+            <div className="journey-picker__label">Kam:</div>
+            <select
+              value={toCity}
+              onChange={(event) => setToCity(event.target.value)}
+            >
               <option value="">Vyberte</option>
               <option value="Mesto1">Město 1</option>
               <option value="Mesto2">Město 2</option>
@@ -29,8 +47,11 @@ export const JourneyPicker = () => {
             </select>
           </label>
           <label>
-            <div class="journey-picker__label">Datum:</div>
-            <select>
+            <div className="journey-picker__label">Datum:</div>
+            <select
+              value={date}
+              onChange={(event) => setDate(event.target.value)}
+            >
               <option value="">Vyberte</option>
               <option>20.05.2021</option>
               <option>21.05.2021</option>
@@ -38,13 +59,13 @@ export const JourneyPicker = () => {
               <option>23.05.2021</option>
             </select>
           </label>
-          <div class="journey-picker__controls">
-            <button class="btn" type="submit">
+          <div className="journey-picker__controls">
+            <button className="btn" type="submit">
               Vyhledat spoj
             </button>
           </div>
         </form>
-        <img class="journey-picker__map" src={mapImage} />
+        <img className="journey-picker__map" src={mapImage} />
       </div>
     </div>
   );
