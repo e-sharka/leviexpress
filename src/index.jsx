2d6b2c8daae1a5eb4fbcc8fa8b297281976923bc
Šárka Princová Maroušková <sarka.marouskova@gmail.com>
2021-05-22 11:14:06 +0200

    Hlavní stránka: Hlavička a patička

--- src/index.jsx
+++ src/index.jsx
@@ -1,20 +1,16 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
 import './style.css';
 
-const App = () => (
-  <div className="container">
-    <header>
-      <div className="logo"></div>
-      <h1>Webová aplikace</h1>
-    </header>
-    <main>
-      <p>Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí <a href="https://www.npmjs.com/package/create-czechitas-app">create-czechitas-app</a>.</p>
-    </main>
-    <footer>
-      <p>Czechitas, Digitální akademie: Web</p>
-    </footer>
-  </div>
-);
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Footer />
+    </>
+  );
+};
 
 render(<App />, document.querySelector('#app'));
